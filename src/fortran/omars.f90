e6aeb0bffbb38ebcfe83abc2d2375a18c75c7048
TimWalter <tim.walter@tum.de>
2024-10-09 21:01:23 +0200

    Fortran progress - Calculate RHS

--- src/fortran/omars.f90
+++ src/fortran/omars.f90
@@ -4,12 +4,10 @@ module omars
     !f2py threadsafef
 contains
     subroutine update_cholesky(chol, update_vectors, multipliers)
-        ! Declar inputs
         real(8), intent(in) :: update_vectors(:, :)
         real(8), intent(in) :: multipliers(2)
         real(8), intent(inout) :: chol(:, :)
 
-        ! Declare local variables
         real(8) :: diag(size(chol, 1))
         real(8) :: u(size(chol, 1), size(update_vectors, 1))
         real(8) :: b(size(chol, 1))
@@ -46,14 +44,11 @@ contains
     end subroutine update_cholesky
 
     subroutine decompose_addition(covariance_addition, eigenvalues, eigenvectors)
-        ! Declare inputs
         real(8), intent(in) :: covariance_addition(:)
 
-        ! Declar outputs
         real(8), intent(out) :: eigenvalues(2)
         real(8), intent(out) :: eigenvectors(size(covariance_addition), 2)
 
-        ! Declare local variables
         real(8) :: eigenvalue_intermediate
         integer :: n, i
 
@@ -73,14 +68,11 @@ contains
     end subroutine decompose_addition
 
     subroutine active_base_indices(where, nbases, result)
-        ! Declare inputs
         integer, intent(in) :: nbases
         logical, intent(in) :: where(:, :)
 
-        ! Declare output
         integer, intent(out) :: result(nbases)
 
-        ! Declare local variables
         integer :: j, n_cols
         logical :: col_has_true(size(where, 2))
 
@@ -93,7 +85,6 @@ contains
     end subroutine active_base_indices
 
     subroutine data_matrix(x, basis_indices, covariates, nodes, hinges, where, result, result_mean)
-        ! Declare inputs
         real(8), intent(in) :: x(:, :)
         integer, intent(in) :: basis_indices(:)
         integer, intent(in) :: covariates(:, :)
@@ -101,11 +92,9 @@ contains
         logical, intent(in) :: hinges(:, :)
         logical, intent(in) :: where(:, :)
 
-        ! Declare output
         real(8), intent(out) :: result(size(x, 1), size(basis_indices))
         real(8), intent(out) :: result_mean(size(basis_indices))
 
-        ! Declare local variables
         integer :: i, basis_idx, func_idx
         real(8) :: intermediate_result(size(x, 1))
 
@@ -133,7 +122,6 @@ contains
 
     subroutine update_init(x, old_node, parent_idx, nbases, covariates, nodes, where, fit_matrix, basis_mean, update, &
             update_mean)
-        ! Declare inputs
         real(8), intent(in) :: x(:, :)
         real(8), intent(in) :: old_node
         integer, intent(in) :: parent_idx
@@ -144,11 +132,9 @@ contains
         real(8), intent(in) :: fit_matrix(:, :)
         real(8), intent(in) :: basis_mean(:)
 
-        ! Declare outputs
         real(8), intent(out) :: update(size(fit_matrix, 1))
         real(8), intent(out) :: update_mean
 
-        ! Declare local variables
         integer :: prod_idx, covariate, n_samples
         real(8) :: new_node
 
@@ -176,7 +162,6 @@ contains
     end subroutine update_init
 
     subroutine calculate_fit_matrix(x, nbases, covariates, nodes, hinges, where, fit_matrix, basis_mean)
-        ! Declare inputs
         real(8), intent(in) :: x(:, :)
         integer, intent(in) :: nbases
         integer, intent(in) :: covariates(:, :)
@@ -184,11 +169,9 @@ contains
         logical, intent(in) :: hinges(:, :)
         logical, intent(in) :: where(:, :)
 
-        ! Declare outputs
         real(8), intent(out) :: fit_matrix(size(x, 1), nbases)
         real(8), intent(out) :: basis_mean(nbases)
 
-        ! Declare local variables
         integer :: basis_indices(nbases)
 
         call active_base_indices(where, nbases, basis_indices)
@@ -198,7 +181,6 @@ contains
 
     subroutine extend_fit_matrix(x, nadditions, fit_matrix, basis_mean, covariates, nodes, hinges, where, &
             fit_matrix_extended, basis_mean_extended)
-        ! Declare inputs
         real(8), intent(in) :: x(:, :)
         integer, intent(in) :: nadditions
         real(8), intent(in) :: fit_matrix(:, :)
@@ -208,11 +190,9 @@ contains
         logical, intent(in) :: hinges(:, :)
         logical, intent(in) :: where(:, :)
 
-        ! Declare outputs
         real(8), intent(out) :: fit_matrix_extended(size(x, 1), size(basis_mean) + nadditions)
         real(8), intent(out) :: basis_mean_extended(size(basis_mean) + nadditions)
 
-        ! Declare local variables
         integer :: indices(size(basis_mean) + nadditions)
         real(8) :: fit_matrix_ext(size(fit_matrix, 1), nadditions)
         real(8) :: basis_mean_ext(nadditions)
@@ -231,4 +211,97 @@ contains
             call data_matrix(x, indices, covariates, nodes, hinges, where, fit_matrix_ext, basis_mean_ext)
         end if
     end subroutine extend_fit_matrix
+
+    subroutine update_fit_matrix(fit_matrix, basis_mean, update, update_mean)
+        real(8), intent(in) :: update(:)
+        real(8), intent(in) :: update_mean
+
+        real(8), intent(inout) :: fit_matrix(:, :)
+        real(8), intent(inout) :: basis_mean(:)
+
+        integer :: nbases
+
+        nbases = size(fit_matrix, 2)
+        fit_matrix(:, nbases) = fit_matrix(:, nbases) + update
+        basis_mean(nbases) = basis_mean(nbases) + update_mean
+
+    end subroutine update_fit_matrix
+
+    subroutine calculate_covariance_matrix(fit_matrix, covariance_matrix)
+        real(8), intent(in) :: fit_matrix(:, :)
+        real(8), intent(out) :: covariance_matrix(size(fit_matrix, 2), size(fit_matrix, 2))
+
+        integer :: i
+
+        covariance_matrix = matmul(transpose(fit_matrix), fit_matrix)
+
+        ! Add epsilon to the diagonal
+        do i = 1, size(fit_matrix, 2)
+            covariance_matrix(i, i) = covariance_matrix(i, i) + 1.0d-8
+        end do
+    end subroutine calculate_covariance_matrix
+
+    subroutine extend_covariance_matrix(covariance_matrix, nadditions, fit_matrix, covariance_matrix_extended)
+        real(8), intent(in) :: covariance_matrix(:, :)
+        integer, intent(in) :: nadditions
+        real(8), intent(in) :: fit_matrix(:, :)
+
+        real(8), intent(out) :: covariance_matrix_extended(size(covariance_matrix, 1) + nadditions, &
+                size(covariance_matrix, 1) + nadditions)
+
+        real(8) :: covariance_extension(size(covariance_matrix, 1), nadditions)
+        integer :: i, nbases
+
+        nbases = size(fit_matrix, 2)
+        covariance_extension = matmul(transpose(fit_matrix), fit_matrix(:, nbases - nadditions:nbases))
+
+        if (size(covariance_matrix, 1) > 0) then
+            covariance_matrix_extended(1:nbases - nadditions, 1:nbases - nadditions) = covariance_matrix
+
+            covariance_matrix_extended(1:nbases - nadditions, nbases - nadditions:nbases) = covariance_extension
+            covariance_matrix_extended(nbases - nadditions:nbases, :) = transpose(covariance_extension)
+        else
+            covariance_matrix_extended = covariance_extension
+        end if
+
+        do i = 0, nadditions - 1
+            covariance_matrix_extended(nbases - i, nbases - i) = covariance_matrix_extended(nbases - i, nbases - i) + 1.0d-8
+        end do
+
+    end subroutine extend_covariance_matrix
+
+    subroutine update_covariance_matrix(covariance_matrix, update, fit_matrix, covariance_addition)
+        real(8), intent(in) :: update(:)
+        real(8), intent(in) :: fit_matrix(:, :)
+        real(8), intent(inout) :: covariance_matrix(:, :)
+
+        real(8), intent(out) :: covariance_addition(size(covariance_matrix, 2))
+
+        integer :: nbases
+
+        nbases = size(covariance_matrix, 2)
+
+        covariance_addition(1:nbases - 1) = matmul(update, fit_matrix(:, 1:nbases - 1))
+        covariance_addition(nbases) = 2.0d0 * dot_product(fit_matrix(:, nbases), update)
+        covariance_addition(nbases) = covariance_addition(nbases) - dot_product(update, update)
+
+        covariance_matrix(nbases, 1:nbases - 1) = covariance_matrix(nbases, 1:nbases - 1) + covariance_addition(1:nbases - 1)
+        covariance_matrix(:, nbases) = covariance_matrix(:, nbases) + covariance_addition
+
+    end subroutine update_covariance_matrix
+
+    subroutine calculate_right_hand_side(y, fit_matrix, right_hand_side, y_mean)
+        real(8), intent(in) :: y(:)
+        real(8), intent(in) :: fit_matrix(:, :)
+
+        real(8), intent(out) :: right_hand_side(size(fit_matrix, 2))
+        real(8), intent(out) :: y_mean
+        real(8) :: y_centred(size(y))
+
+        y_mean = sum(y) / real(size(y, 1))
+        y_centred = y - y_mean
+        right_hand_side = matmul(transpose(fit_matrix), y_centred)
+
+    end subroutine calculate_right_hand_side
+
 end module omars
