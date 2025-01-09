2b308442e9662e1c9514584fb115271f1afa1e0c
TimWalter <tim.walter@tum.de>
2025-01-09 21:28:55 +0100

    Scaling laws

--- tests/speed_test.py
+++ tests/speed_test.py
@@ -55,29 +55,28 @@ model = omars.OMARS(max_nbases=max_nbases, max_ncandidates=5, backend=backend)
         N_times = []
         for n_samples in N:
             variables["n_samples"] = n_samples
-            N_times += [np.mean(timeit.repeat(command, setup=setup, globals=variables, repeat=10, number=1))]
+            N_times.append(np.mean(timeit.repeat(command, setup=setup, globals=variables, repeat=10, number=1)))
 
-        results[backend]["n_samples"] = np.array([[N], [N_times]])
+        results[backend]["n_samples"] = (N, N_times)
 
         M = np.linspace(3, 17, 8, dtype=int)
         M_times = []
         for m_max in M:
             variables["max_nbases"] = m_max
-            M_times += [np.mean(timeit.repeat(command, setup=setup, globals=variables, repeat=10, number=1))]
-        results[backend]["max_nbases"] = np.array([[M], [M_times]])
+            M_times.append(np.mean(timeit.repeat(command, setup=setup, globals=variables, repeat=10, number=1)))
+        results[backend]["max_nbases"] = (M, M_times)
         variables["max_nbases"] = 11
 
         d = np.linspace(2, 10, 5, dtype=int)
         d_times = []
         for dim in d:
             variables["dim"] = dim
-            d_times += [np.mean(timeit.repeat(command, setup=setup, globals=variables, repeat=10, number=1))]
-        results[backend]["dim"] = np.array([[d], [d_times]])
+            d_times.append(np.mean(timeit.repeat(command, setup=setup, globals=variables, repeat=10, number=1)))
+        results[backend]["dim"] = (d, d_times)
 
     with open("../benchmark/scaling_laws.csv", "w", newline='') as file:
         writer = csv.writer(file)
         writer.writerow(["Backend", "Parameter", "Values", "Times"])
         for backend, params in results.items():
-            for param, data in params.items():
-                values, times = data
-                writer.writerow([backend, param, values.tolist(), times.tolist()])
+            for param, (values, times) in params.items():
+                writer.writerow([backend, param, list(values), list(times)])
